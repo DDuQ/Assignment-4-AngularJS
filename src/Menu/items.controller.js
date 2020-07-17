8f2c363afe8c05d60f622ef64507c4581e1bde9d
DDuQ <estebanduque2@gmail.com>
2020-07-16 19:18:34 -0500

    task finished

---
+++ src/Menu/items.controller.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+angular.module('MenuApp')
+.controller('ItemsController', ItemsController);
+
+//'items' ref'd from state's resolve in route.js
+ItemsController.$inject = ['$stateParams','items'];
+function ItemsController($stateParams, items) {
+   var Itmctrl = this;
+   Itmctrl.items = items;
+}
+
+}());
